9ec71555083a2610861734baf2aa56a123e4dd9e
JuliaGolban <julia.j.shcherban@gmail.com>
2023-07-06 12:27:20 +0300

    add services styles

--- src/components/baseStyles/CommonStyle.styled.js
+++ src/components/baseStyles/CommonStyle.styled.js
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100%;
   margin: 0 auto;
-  padding: 0 20px;
+  padding: 30px;
 
   @media screen and (min-width: 768px) {
-    padding: 0 40px;
+    padding: 60px 90px;
   }
 
   @media screen and (min-width: 1280px) {
-    max-width: 1200px;
+    padding: 100px;
+    max-width: 1280px;
   }
 `;
 
@@ -21,18 +22,19 @@ const Title = styled.h1`
   font-size: 32px;
   font-style: normal;
   font-weight: 900;
-  line-height: 105px;
+  line-height: 1.373;
   letter-spacing: -2.56px;
   text-transform: uppercase;
   background: linear-gradient(rgba(65, 45, 183, 1), rgba(219, 47, 47, 1) 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
   background-clip: text;
   color: transparent;
 
   @media screen and (min-width: 768px) {
     margin: 0 0 45px 0;
     font-size: 55px;
-    line-height: 65px;
-    letter-spacing: -6.88px;
+    letter-spacing: -4.88px;
   }
 
   @media screen and (min-width: 1280px) {
