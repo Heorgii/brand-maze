9822a4dddafae15a8a27349aeb4d2292e25164e5
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-07-06 11:26:51 +0300

    add title for pics

--- src/components/Sections/Projects/Projects.jsx
+++ src/components/Sections/Projects/Projects.jsx
@@ -12,18 +12,26 @@ export const Project = () => {
     {
       original: Barber_img,
       thumbnail: Barber_img,
+      description:"A convenient service for hairdressers, massage salons, which helps users make online reservations for the selected service",
+      originalTitle:"A convenient service for hairdressers, massage salons, which helps users make online reservations for the selected service",
     },
     {
       original: Animal_welfare_img,
       thumbnail: Animal_welfare_img,
+      description:"A project that helps to find a pet for youself or place a homeless animal in good hands",
+      originalTitle:"A project that helps to find a pet for youself or place a homeless animal in good handsProject #2",
     },
     {
       original: Filmoteka_img,
       thumbnail: Filmoteka_img,
+      description:"This site helps to choose movies or cartoons for viewing by the whole family and records the preferences of each individual member of the family",
+      originalTitle:"This site helps to choose movies or cartoons for viewing by the whole family and records the preferences of each individual member of the family",
     },
     {
       original: Ice_cream_img,
       thumbnail: Ice_cream_img,
+      description:"A project for ordering delicious and cold ice cream",
+      originalTitle:"A project for ordering delicious and cold ice cream",
     },
   ];
 
@@ -31,7 +39,7 @@ export const Project = () => {
     <Container>
       <Title>EXPLORE OUR WORKS</Title>
       <SlideContainer>
-        <ImageGallery autoPlay={true} items={images} />;
+        <ImageGallery autoPlay={true} items={images} lazyLoad={true} description={images} slideDuration={700} originalTitle={images}/>;
       </SlideContainer>
     </Container>
   );
