6973b386be23d512dcbf671192458baa803551f4
JuliaGolban <julia.j.shcherban@gmail.com>
2023-07-07 19:52:52 +0300

    fix contact page

--- src/components/Sections/Contact/Contact.jsx
+++ src/components/Sections/Contact/Contact.jsx
@@ -1,5 +1,16 @@
 import { useState } from 'react';
-import { ContactForm, LabelOfItem, InputOfItem, TextareaOfItem, ButtonContainer, InputForFile, Button, ContainerForInputForFile, LabelForInputForFile} from './Contact.styled';
+import {
+  ContactForm,
+  InputWrapper,
+  LabelOfItem,
+  InputOfItem,
+  NameOfItem,
+  TextareaOfItem,
+  InputForFile,
+  ContainerForInputForFile,
+  LabelForInputForFile,
+  ButtonSend,
+} from './Contact.styled';
 import { Container, Title } from 'components/baseStyles/CommonStyle.styled';
 
 export const Contact = () => {
@@ -10,138 +21,161 @@ export const Contact = () => {
   const [userMessage, setUserMessage] = useState('');
   const [file, setFile] = useState('');
 
-document.querySelector("html").classList.add('js');
-console.log(file);
+  document.querySelector('html').classList.add('js');
+  console.log(file);
 
-const handleSubmit = () => {
-  console.log(`send:`,userFirstName, userLastName, userEmail, userPhone, userMessage, file);
-  setUserFirstName('');
-  setUserLastName('');
-  setUserEmail('');
-  setUserPhone('');
-  setUserMessage('');
-  setFile('');
-  document.querySelectorAll('.file-return')[0].innerHTML = '(Allowed file formats - pdf doc docx odt ods. Maximum file size - 5 mb)'
-}
+  const handleSubmit = () => {
+    setUserFirstName('');
+    setUserLastName('');
+    setUserEmail('');
+    setUserPhone('');
+    setUserMessage('');
+    setFile('');
+    document.querySelectorAll('.file-return')[0].innerHTML =
+      '(Allowed file formats - pdf doc docx odt ods. Maximum file size - 5 mb)';
+  };
 
-const handleChange = (e) => {
-  e.preventDefault();
-  switch(e.target.name){
-    case "user-firstname": setUserFirstName(e.target.value);
-    break;
-    case "user-lastname": setUserLastName(e.target.value);
-    break;
-    case "user-email": setUserEmail(e.target.value);
-    break;
-    case "user-phone": setUserPhone(e.target.value);
-    break;
-    case "comment": setUserMessage(e.target.value);
-    break;
-    default: 
-    break;
-  }
-}
+  const handleChange = e => {
+    e.preventDefault();
+    switch (e.target.name) {
+      case 'user-firstname':
+        setUserFirstName(e.target.value);
+        break;
+      case 'user-lastname':
+        setUserLastName(e.target.value);
+        break;
+      case 'user-email':
+        setUserEmail(e.target.value);
+        break;
+      case 'user-phone':
+        setUserPhone(e.target.value);
+        break;
+      case 'comment':
+        setUserMessage(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
 
-  return <Container>
-    <Title>Contact</Title>
-    <ContactForm
-            className="contact-form"
-            name="form-contacts"
-            autoComplete="on"
-            onSubmit={e => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            <LabelOfItem aria-label="Name">
-            <span>First Name</span>
-              <br/><InputOfItem
-                type="text"
-                name="user-firstname"
-                required
-                placeholder="John"
-                value={userFirstName}
-                onChange={e => handleChange(e)}
-              />
-            </LabelOfItem>
-            <LabelOfItem  aria-label="LastName">
-            <span>Last Name</span>
-            <br/>
-              <InputOfItem
-                type="text"
-                name="user-lastname"
-                required
-                placeholder="Watson"
-                value={userLastName}
-                onChange={e => handleChange(e)}
-              />
-            </LabelOfItem>
-            <LabelOfItem  aria-label="Email">
-            <span>Email</span>
-            <br/>
-              <InputOfItem
-                type="email"
-                name="user-email"
-                required
-                placeholder="bob@gmail.com"
-                value={userEmail}
-                onChange={e => handleChange(e)}
-              />
-            </LabelOfItem>
-            <LabelOfItem  aria-label="Phone">
-            <span>Phone number</span>
-            <br/>
-              <InputOfItem
-                type="tel"
-                name="user-phone"
-                required
-                placeholder="+19739476185"
-                value={userPhone}
-                onChange={e => handleChange(e)}
-              />
-
-            </LabelOfItem>
-            <LabelOfItem
-              className="comment"
-              aria-label="Comment"
-              style={{width:"100%"}}
+  return (
+    <Container>
+      <Title>Contact</Title>
+      <ContactForm
+        className="contact-form"
+        name="form-contacts"
+        autoComplete="on"
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <InputWrapper>
+          <LabelOfItem aria-label="Name">
+            <InputOfItem
+              type="text"
+              name="user-firstname"
+              required
+              placeholder="John"
+              value={userFirstName}
+              onChange={e => handleChange(e)}
+            />
+            <NameOfItem>First Name</NameOfItem>
+          </LabelOfItem>
+          <LabelOfItem aria-label="LastName">
+            <InputOfItem
+              type="text"
+              name="user-lastname"
+              required
+              placeholder="Watson"
+              value={userLastName}
+              onChange={e => handleChange(e)}
+            />
+            <NameOfItem>Last Name</NameOfItem>
+          </LabelOfItem>
+          <LabelOfItem aria-label="Email">
+            <InputOfItem
+              type="email"
+              name="user-email"
+              required
+              placeholder="bob@email.com"
+              value={userEmail}
+              onChange={e => handleChange(e)}
+            />
+            <NameOfItem>Email</NameOfItem>
+          </LabelOfItem>
+          <LabelOfItem aria-label="Phone">
+            <InputOfItem
+              type="tel"
+              name="user-phone"
+              required
+              placeholder="+19739476185"
+              value={userPhone}
+              onChange={e => handleChange(e)}
+            />
+            <NameOfItem>Phone number</NameOfItem>
+          </LabelOfItem>
+        </InputWrapper>
+        <LabelOfItem
+          className="comment"
+          aria-label="Comment"
+          style={{ width: '100%' }}
+        >
+          <TextareaOfItem
+            name="comment"
+            placeholder="Write your message"
+            value={userMessage}
+            onChange={e => handleChange(e)}
+          />
+          <NameOfItem>Message</NameOfItem>
+        </LabelOfItem>
+        <ContainerForInputForFile>
+          <div className="input-file-container">
+            <InputForFile
+              className="input-file"
+              id="my-file"
+              type="file"
+              accept="image/png, image/jpeg, .pdf, .doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              onChange={e => {
+                if (e.target.value === '' || e.target.value === undefined) {
+                  document.querySelectorAll(
+                    '.input-file-trigger'
+                  )[0].innerHTML = 'Add a file...';
+                  document.querySelectorAll('.file-return')[0].innerHTML =
+                    '(Allowed file formats - pdf doc docx odt ods. Maximum file size - 5 mb)';
+                } else {
+                  document.querySelectorAll(
+                    '.input-file-trigger'
+                  )[0].innerHTML = 'File added';
+                  document.querySelectorAll('.file-return')[0].innerHTML =
+                    e.target.value;
+                }
+                setFile(e.target.files[0]);
+              }}
+            />
+            <LabelForInputForFile
+              tabIndex="0"
+              htmlFor="my-file"
+              className="input-file-trigger"
+              onClick={e => {
+                document.querySelectorAll('.input-file').focus();
+              }}
+              onKeyDown={e => {
+                if (e.key === 13 || e.key === 32) {
+                  document.querySelectorAll('.input-file').focus();
+                }
+              }}
             >
-            <span>Message</span>
-            <br/>
-              <TextareaOfItem
-                name="comment"
-                placeholder="Write your message"
-                value={userMessage}
-                onChange={e => handleChange(e)}
-              ></TextareaOfItem>
-             </LabelOfItem>
-             <ButtonContainer>
-              <ContainerForInputForFile>  
-             <div className="input-file-container">
-                <InputForFile 
-                  className="input-file" id="my-file" 
-                  type="file" accept="image/png, image/jpeg, .pdf, .doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" onChange={(e)=>{ if(e.target.value === '' || e.target.value === undefined){document.querySelectorAll('.input-file-trigger')[0].innerHTML = 'Add a file...'; 
-                  document.querySelectorAll('.file-return')[0].innerHTML = '(Allowed file formats - pdf doc docx odt ods. Maximum file size - 5 mb)'} else {
-                    document.querySelectorAll('.input-file-trigger')[0].innerHTML = 'File added'; 
-                    document.querySelectorAll('.file-return')[0].innerHTML = e.target.value};
-                    setFile(e.target.files[0])}}/>
-                <LabelForInputForFile tabIndex="0" htmlFor="my-file" 
-                className="input-file-trigger" 
-                onClick={(e)=>{document.querySelectorAll( ".input-file" ).focus()}}
-                onKeyDown={(e)=>{if ( e.key === 13 || e.key === 32 ) { document.querySelectorAll( ".input-file" ).focus(); }
-                }}>Add a file...
-                </LabelForInputForFile>
-            </div>  
-            <Button style={{width:"156px", height:"40px"}}
-              type="submit"
-            >
-              Send message
-            </Button>
-            </ContainerForInputForFile>  
-            <ContainerForInputForFile>  
-            <p className="file-return">(Allowed file formats - pdf doc docx odt ods. Maximum file size - 5 mb)</p>
-            </ContainerForInputForFile>
-            </ButtonContainer>
-          </ContactForm>
-  </Container>;
+              Add a file...
+            </LabelForInputForFile>
+          </div>
+          <p className="file-return">
+            (Allowed file formats - pdf doc docx odt ods. Maximum file size - 5
+            mb)
+          </p>
+        </ContainerForInputForFile>
+        <ButtonSend type="submit">Send message</ButtonSend>
+      </ContactForm>
+    </Container>
+  );
 };
--- src/components/Sections/Contact/Contact.styled.js
+++ src/components/Sections/Contact/Contact.styled.js
@@ -1,148 +1,255 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { Button } from 'components/baseStyles/Button.styled';
 
 const ContactForm = styled.form`
-  display: flex;
   position: relative;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
-  font-family: "Roboto", sans-serif;
+
+  width: 100%;
+  margin-top: 25px;
+  padding: 0 30px;
+
+  font-family: 'Roboto', sans-serif;
   font-style: normal;
-  width: 600px;
+
   overflow: hidden;
-  padding: 20px 30px;
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    width: 300px;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 65px;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-between;
+
+  width: 100%;
+  margin-bottom: 30px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    gap: 30px;
   }
 `;
 
 const LabelOfItem = styled.label`
+  position: relative;
+  width: calc(50% - 30px);
+  margin-bottom: 30px;
+
   font-weight: 600;
   font-size: 11px;
   line-height: 1.36;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  width: calc(100% / 2);
-  color: rgba(157, 164, 189, 0.6);
+  color: ${props => props.theme.form};
+
   cursor: pointer;
-  margin-bottom: 30px;
+
   @media screen and (max-width: 768px) {
-    width: calc(100%);
+    width: 100%;
   }
 `;
 
 const InputOfItem = styled.input`
+  width: 100%;
+  height: 40px;
+  min-width: 250px;
+  padding-top: 14px;
+
   border: none;
   outline: none;
-  border-bottom: 2px solid black;
-  width: calc(100% / 2);
-  color: rgba(100, 100, 100, 0.9);
+  border-bottom: 1px solid ${props => props.theme.form};
+  color: ${props => props.theme.black};
+  background-color: transparent;
   cursor: pointer;
-  padding-top: 10px;
-  min-width: 250px;
+
   @media screen and (max-width: 768px) {
-    width: calc(100%);
+    width: 100%;
+  }
+
+  &:hover,
+  &:focus,
+  &:active {
+    outline: 0;
+    border-bottom: 1px solid ${props => props.theme.black};
+  }
+
+  &::placeholder {
+    opacity: 0;
+    transition: opacity 150ms linear;
+  }
+
+  &:hover::placeholder,
+  &:focus::placeholder {
+    opacity: 1;
+  }
+
+  &:hover ~ span,
+  &:focus ~ span,
+  &:not(:placeholder-shown) ~ span {
+    color: ${props => props.theme.form};
+    transform: translateY(0);
   }
 `;
-const TextareaOfItem = styled.textarea`
-  border: none;
-  outline: none;
-  border-bottom: 2px solid black;
-  width: calc(100%);
-  color: rgba(100, 100, 100, 0.9);
-  cursor: pointer;
+
+const NameOfItem = styled.span`
+  position: absolute;
+  top: 0px;
+  left: 2px;
+
+  font-weight: 500;
+  font-size: 11px;
+  line-height: 1.36;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+
+  color: ${props => props.theme.form};
+
+  transform: translateY(20px);
+  transition: transform 150ms linear;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  position: relative;
-  flex-direction: column;
+const TextareaOfItem = styled.textarea`
   width: 100%;
-  justify-content: space-around;
-  align-items: center;
-  flex-wrap: nowrap;
-  font-family: "Roboto", sans-serif;
-  font-style: normal;
+  height: 40px;
+  padding-top: 20px;
+
+  color: ${props => props.theme.form};
+  background-color: transparent;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid ${props => props.theme.form};
+  cursor: pointer;
+
+  transition: border-color 150ms linear;
+
+  &:hover,
+  &:focus,
+  &:active {
+    outline: 0;
+    border: none;
+    border-bottom: 1px solid ${props => props.theme.black};
+  }
+
+  &::placeholder {
+    opacity: 0;
+    transition: opacity 150ms linear;
+  }
+
+  &:hover::placeholder,
+  &:focus::placeholder {
+    opacity: 1;
+  }
+
+  &:hover ~ span,
+  &:focus ~ span,
+  &:not(:placeholder-shown) ~ span {
+    color: ${props => props.theme.form};
+    transform: translateY(0);
+  }
 `;
+
 const ContainerForInputForFile = styled.div`
+  position: relative;
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
   width: 100%;
+
+  & .file-return {
+    font-style: normal;
+    font-size: 0.7em;
+    font-weight: 400;
+    color: ${props => props.theme.black};
+  }
 `;
+
 const InputForFile = styled.input`
   width: 200px;
   height: 50px;
   margin: 0 auto;
   display: block;
-  color: #fff;
+
   text-align: center;
   font-size: 16px;
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
-  background: #000;
+
+  color: ${props => props.theme.white};
+  background: ${props => props.theme.black};
+
   border-radius: 3px;
+
   @media screen and (max-width: 768px) {
     width: 100px;
     font-size: 10px;
+  }
+
+  & ~ label {
+    margin: 0;
   }
 `;
 const LabelForInputForFile = styled.label`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: black;
-  color: white;
-  font-size: 1em;
-  transition: all 0.4s;
-  cursor: pointer;
+
   width: 156px;
   height: 40px;
   margin: 0 auto;
+
   font-size: 16px;
-  font-family: "Roboto", sans-serif;
+  font-family: 'Roboto', sans-serif;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
+  color: ${props => props.theme.white};
+
+  background: ${props => props.theme.black};
   border-radius: 3px;
+
+  transition: all 150ms linear;
+  cursor: pointer;
+
   @media screen and (max-width: 768px) {
     width: 100px;
     font-size: 10px;
   }
 `;
 
-const Button = styled.button`
+const ButtonSend = styled(Button)`
   width: 156px;
   height: 40px;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #fff;
-  text-align: center;
+  margin: 20px 0 0 auto;
   font-size: 16px;
-  font-family: "Roboto", sans-serif;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  background: #000;
-  border-radius: 3px;
+
   @media screen and (max-width: 768px) {
     width: 100px;
+    height: 40px;
     font-size: 10px;
   }
 `;
 
 export {
   ContactForm,
+  InputWrapper,
   LabelOfItem,
   InputOfItem,
+  NameOfItem,
   TextareaOfItem,
-  ButtonContainer,
   InputForFile,
-  Button,
+  ButtonSend,
   ContainerForInputForFile,
   LabelForInputForFile,
 };
