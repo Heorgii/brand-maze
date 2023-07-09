4165a69cd0c8750781e4fca10eb8e1f3e66f5751
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-07-09 22:38:35 +0300

    Update Projects.jsx

--- src/components/Sections/Projects/Projects.jsx
+++ src/components/Sections/Projects/Projects.jsx
@@ -6,7 +6,7 @@ import Animal_welfare_img from 'images/projects/animal_welfare_project.webp';
 import Filmoteka_img from 'images/projects/filmoteka_project.webp';
 import Ice_cream_img from 'images/projects/ice_cream_project.webp';
 import { Container, Title } from 'components/baseStyles/CommonStyle.styled';
-import { Button } from 'components/baseStyles/Button.styled';
+// import { Button } from 'components/baseStyles/Button.styled';
 import { useEffect } from 'react';
 import { ButtonLink } from 'components/baseStyles/Button.styled';
 
