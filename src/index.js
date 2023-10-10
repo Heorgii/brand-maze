92f1dc4e7d7bb43d11f2c7485e6aa47a8f9c7d3c
Heorgii <103379960+Heorgii@users.noreply.github.com>
2023-10-10 17:44:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import i18n from './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="brand-maze">
+    <BrowserRouter basename="/">
       <I18nextProvider i18n={i18n}>
         <ThemeStatus>
           <GlobalStyle />
